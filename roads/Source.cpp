33c2bb99d0be85e894449bb3b0205ec2c7cefa5c
anastasiaspakovskaa <anastasiaspakovskaa733@gmail.com>
2022-04-19 16:25:33 +0300

    Time Limit Exceeded

--- roads/Source.cpp
+++ roads/Source.cpp
@@ -3,6 +3,33 @@
 
 using namespace std;
 
+int findSet(int u, int* set) {
+	int repU = set[u];
+	while (repU != set[repU]) {
+		repU = set[repU];
+	}
+	return repU;
+}
+
+bool unionSet(int u, int v, int* set, int* size) {
+	int repU = findSet(u, set);
+	int repV = findSet(v, set);
+	if (repU != repV) {
+		if (size[repU] > size[repV]) {
+			set[repV] = repU;
+			size[repU] += size[repV];
+		}
+		else {
+			set[repU] = repV;
+			size[repV] += size[repU];
+		}
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
 int main() {
 	fstream in;
 	in.open("input.txt", ios::in);
@@ -21,21 +48,19 @@ int main() {
 	in >> n >> q;
 
 	int* set = new int[n + 1];
+	int* size = new int[n + 1];
 
 	for (int i = 1; i < n + 1; i++) {
 		set[i] = i;
+		size[i] = 1;
 	}
 
-	int u, v, num = n;
+	int u, v;
+	int num = n;
 
 	for (int i = 0; i < q; i++) {
 		in >> u >> v;
-		if (set[u] != set[v]) {
-			for (int i = 1; i < n + 1; i++) {
-				if (set[i] == set[u]) {
-					set[i] = set[v];
-				}
-			}
+		if (unionSet(u, v, set, size)) {
 			num--;
 		}
 		out << num << "\n";
