ca7cfc340bc4ee821e91159b536b3f90db8aabf7
anastasiaspakovskaa <anastasiaspakovskaa733@gmail.com>
2022-06-15 22:58:03 +0300

    .

--- roads/Source.cpp
+++ roads/Source.cpp
@@ -11,18 +11,11 @@ int findSet(int u, int* set) {
 	return repU;
 }
 
-bool unionSet(int u, int v, int* set, int* size) {
+bool unionSet(int u, int v, int* set) {
 	int repU = findSet(u, set);
 	int repV = findSet(v, set);
 	if (repU != repV) {
-		if (size[repU] > size[repV]) {
-			set[repV] = repU;
-			size[repU] += size[repV];
-		}
-		else {
-			set[repU] = repV;
-			size[repV] += size[repU];
-		}
+		set[repV] = repU;
 		return true;
 	}
 	else {
@@ -33,26 +26,18 @@ bool unionSet(int u, int v, int* set, int* size) {
 int main() {
 	fstream in;
 	in.open("input.txt", ios::in);
-	if (!in.is_open()) {
-		cout << "file is not open" << endl;
-	}
 
 	ofstream out;
 	out.open("output.txt", ios::out);
-	if (!out.is_open()) {
-		cout << "file is not open" << endl;
-	}
 
 	int n, q;
 
 	in >> n >> q;
 
 	int* set = new int[n + 1];
-	int* size = new int[n + 1];
 
 	for (int i = 1; i < n + 1; i++) {
 		set[i] = i;
-		size[i] = 1;
 	}
 
 	int u, v;
@@ -60,7 +45,7 @@ int main() {
 
 	for (int i = 0; i < q; i++) {
 		in >> u >> v;
-		if (unionSet(u, v, set, size)) {
+		if (unionSet(u, v, set)) {
 			num--;
 		}
 		out << num << "\n";
